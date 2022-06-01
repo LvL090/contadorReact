92da723f5b51cc22be0f3b79f88d278bde65e221
LvL090 <calvod879@gmail.com>
2022-06-01 12:55:47 +0200

    udate2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
-
+import logo from './img/logo.jpg';
+import './App.css';
 
 
 function App() {
 return (
 
   <div className="App">
-      <div className='logo'>
+      <div className='logo object-contain h-48 w-96'>
         <img
           className='logo'
-          src ={}
+          src ={logo}
           alt='logo' />
       </div>
   </div>
